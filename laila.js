f3b0cb059d8ddc6e13b40306d3bdc948c3ff5607
LailaHussien2005 <lailahussien274@gmail.com>
2024-10-29 14:18:20 +0300

    first commint

---
+++ laila.js
@@ -0,0 +1,35 @@
+
+let body = document.body;
+let darkMode = document.getElementById('darkmode');
+darkMode.addEventListener('click', function () {
+    darkMode.classList.toggle('active');
+    body.classList.toggle('night');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
